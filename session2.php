2a42d1585074ba5c2b158219047579907d8de911
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-11-02 18:05:09 +0100

    Cookies
    Set Cookies and Read

---
+++ session2.php
@@ -0,0 +1,19 @@
+<?php session_start();
+
+ echo $_SESSION['greeting'];
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>session2.php</title>
+  </head>
+  <body>
+
+  </body>
+</html>
